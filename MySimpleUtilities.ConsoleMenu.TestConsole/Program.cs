9b07ea21e2892f260ded006c2eb64a44ae2b59e6
Josh Tomlinson <ycland@gmail.com>
2017-04-05 13:13:03 -0700

    finalized some fixes for getinput variants

--- MySimpleUtilities.ConsoleMenu.TestConsole/Program.cs
+++ MySimpleUtilities.ConsoleMenu.TestConsole/Program.cs
@@ -10,52 +10,12 @@ namespace MySimpleUtilities.ConsoleMenu.TestConsole
     {
         static void Main(string[] args)
         {
-            //var menu = new ConsoleMenu()
-            //{
-            //    HeaderText = "header text",
-            //    PostActionAction = () => { Console.WriteLine("Press any key to continue..."); Console.ReadKey(); },
-            //    SubHeaderText = "sub header text",
-            //    EscapeClosesMenu = true
-            //};
-            //menu.AddMenuItem(new MenuItem
-            //{
-            //    Text = "MenuItem 1",
-            //    Action = () => Console.WriteLine("Menu Item 1 selected"),
-            //    ValidItem = true
-            //});
-            //menu.AddMenuItem(new MenuItem
-            //{
-            //    Text = "Invalid Menu Item",
-            //    ValidItem = false
-            //});
-            //menu.AddMenuItem(new MenuItem
-            //{
-            //    Text = "MenuItem 2",
-            //    Action = () => Console.WriteLine("Menu Item 2 selected"),
-            //    ValidItem = true
-            //});
-            //menu.AddMenuItem(new MenuItem()
-            //{
-            //    Text = "MenuItem 3",
-            //    Action = () => { Console.WriteLine("Menu Item 3 selected"); menu.DeleteCurrentMenuItem(); },
-            //    ValidItem = true
-            //});
-            //menu.ShowMenu();
-
-            //var input = ConsoleUtil.GetInput<bool>("test");
-            //Console.WriteLine(input);
-
-            //if (ConsoleUtil.GetValidatedEscapableInput<int>("Give me an int between 5-25.", x => x >= 5 && x <= 25, out int value))
-            //    Console.WriteLine(value);
-            //else
-            //    Console.WriteLine("Escaped");
-
-            //if (ConsoleUtil.GetValidatedEscapableInput<string>("Give me a string with length greater than 5", x => x.Length > 5, out string result))
-            //    Console.WriteLine($"String: { result }{ Environment.NewLine }Length: { result.Length }");
-            //else
-            //    Console.WriteLine("Escaped");
-
-
+            if (ConsoleUtil.GetValidatedEscapableInput<int>("5 < int < 10", x => x > 5 && x < 10, out int value))
+            {
+                Console.WriteLine("Good! " + value);
+            }
+            else
+                Console.WriteLine("Canceled input");
 
             Console.ReadKey();
         }
--- MySimpleUtilities/ConsoleGoodies/ConsoleUtil.cs
+++ MySimpleUtilities/ConsoleGoodies/ConsoleUtil.cs
@@ -1,5 +1,4 @@
-﻿using MySimpleUtilities.ConsoleGoodies.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,81 +20,10 @@ namespace MySimpleUtilities.ConsoleMenu
             WriteColor(_p + Environment.NewLine, _color);
         }
 
-        public static EscapableInputReturnModel<T> GetValidatedEscapableInput<T>(string prompt, Func<T, bool> validator, string retry = "Invalid Input")
-        {
-            T value;
-            var returnModel = new EscapableInputReturnModel<T>();
-            bool isValid;
-
-            try
-            {
-                do
-                {
-                    var result = GetEscapableInput<T>(prompt, retry);
-                    if (result.WasEscaped)
-                    {
-                        returnModel.WasEscaped = result.WasEscaped;
-                        isValid = true;
-                        return returnModel;
-                    }
-                    else
-                    {
-                        value = (T)Convert.ChangeType(result.Value, typeof(T));
-                        isValid = validator(value);
-                    }
-
-                    if (!isValid)
-                        Console.WriteLine(retry);
-
-                } while (!isValid);
-                returnModel.Value = value;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine(retry);
-                GetValidatedEscapableInput(prompt, validator, retry);
-            }
-
-            return returnModel;
-        }
-        public static EscapableInputReturnModel<T> GetEscapableInput<T>(string _prompt, string _retry = "Invalid Input")
-        {
-            Console.Write($"{ _prompt }: ");
-            ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
-            var sb = new StringBuilder();
-            var returnModel = new EscapableInputReturnModel<T>();
-            bool escChar;
-            T value = default(T);
-
-            try
-            {
-                do
-                {
-                    keyInfo = Console.ReadKey();
-                    sb.Append(keyInfo.KeyChar);
-                    escChar = keyInfo.Key == ConsoleKey.Enter || keyInfo.Key == ConsoleKey.Escape;
-                } while (!escChar);
-                if (keyInfo.Key != ConsoleKey.Escape)
-                    value = (T)Convert.ChangeType(sb.ToString(), typeof(T));
-                else
-                    returnModel.WasEscaped = true;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine(_retry);
-                GetEscapableInput<T>(_prompt, _retry);
-            }
-
-            returnModel.Value = value;
-            Console.Write(Environment.NewLine);
-
-            return returnModel;
-        }
         public static T GetInput<T>(string _prompt, string _retry = "Invalid Input")
         {
             T value = default(T);
             var isBool = typeof(T) == typeof(bool);
-            
             string input = string.Empty;
             var prompt = _prompt + (isBool ? " (y/n)" : "");
 
@@ -121,18 +49,90 @@ namespace MySimpleUtilities.ConsoleMenu
 
             return value;
         }
-
-        public static bool GetValidatedEscapableInput<T>(string _prompt, Func<T, bool> _validator, out T _value, string _retry = "Invalid Input")
+        public static T GetValidatedInput<T>(string _prompt, Func<T, bool> _validator, string _retry = "Invalid Input")
         {
-            var result = GetValidatedEscapableInput<T>(_prompt, _validator, _retry);
-            _value = result.Value;
-            return !result.WasEscaped;
+            T value = default(T);
+            var isBool = typeof(T) == typeof(bool);
+            bool isValid = false;
+
+            try
+            {
+                do
+                {
+                    Console.Write($"{ _prompt }: ");
+                    value = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+                    isValid = _validator(value);
+                    if(!isValid)
+                        Console.WriteLine(_retry);
+                } while (!isValid);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(_retry);
+                GetValidatedInput<T>(_prompt, _validator, _retry);
+            }
+            return value;
         }
+        
         public static bool GetEscapableInput<T>(string _prompt, out T _value, string _retry = "Invalid Input")
         {
-            var result = GetEscapableInput<T>(_prompt, _retry);
-            _value = result.Value;
-            return result.WasEscaped;
+            string inputString;
+            _value = default(T);
+            bool isEmptyInput = false;
+            bool isBool = typeof(T) == typeof(bool);
+
+            try
+            {
+                Console.Write($"{ _prompt }");
+                if (isBool)
+                {
+                    Console.Write($" (y/n): ");
+                    inputString = Console.ReadKey().Key.ToString();
+                    if (string.IsNullOrEmpty(inputString))
+                    {
+                        inputString = string.Empty;
+                        isEmptyInput = true;
+                    }
+                    else if (inputString.Equals("y", StringComparison.OrdinalIgnoreCase))
+                        inputString = true.ToString();
+                    else if (inputString.Equals("n", StringComparison.OrdinalIgnoreCase))
+                        inputString = false.ToString();
+                }
+                else
+                {
+                    Console.Write(": ");
+                    inputString = Console.ReadLine();
+                }
+
+                if (!string.IsNullOrWhiteSpace(inputString))
+                    _value = (T)Convert.ChangeType(inputString, typeof(T));
+                else
+                    isEmptyInput = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(_retry);
+                return GetEscapableInput<T>(_prompt, out _value, _retry);
+            }
+
+            return !isEmptyInput;
+        }
+        public static bool GetValidatedEscapableInput<T>(string _prompt, Func<T, bool> _validator, out T _value, string _retry = "Invalid Input")
+        {
+            bool isValid = false;
+            bool notEscaped = false;
+            do
+            {
+                notEscaped = GetEscapableInput<T>(_prompt, out _value, _retry);
+                if (notEscaped)
+                    isValid = _validator(_value);
+
+                if (!isValid && notEscaped)
+                    Console.WriteLine(_retry);
+
+            } while (!isValid && notEscaped);
+
+            return notEscaped;
         }
     }
 }
